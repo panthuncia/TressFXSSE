d6c8db4a7016d2f9bc012ecf4a5180a70ee0d20a
Matthew Gomes <matthew.h.gomes@gmail.com>
2023-05-16 21:56:54 -0400

    continued

--- src/SkyrimGPUInterface.cpp
+++ src/SkyrimGPUInterface.cpp
@@ -257,8 +257,17 @@ extern "C"
 		ID3DX11Effect* pEffect = (ID3DX11Effect*)&layoutManager;
 		logger::info("1");
 		for (int i = 0; i < description.nSRVs; ++i) {
+			auto var = pEffect->GetVariableByIndex(i);
+			D3DX11_EFFECT_VARIABLE_DESC desc;
+			var->GetDesc(&desc);
+			logger::info("{}", desc.Name);
+		}
+		for (int i = 0; i < description.nSRVs; ++i) {
 			logger::info("Getting SRV: {}", description.srvNames[i]);
-			pLayout->srvs.push_back(pEffect->GetVariableByName(description.srvNames[i])->AsShaderResource());
+			auto var = pEffect->GetVariableByName(description.srvNames[i]);
+			logger::info("1.1");
+			pLayout->srvs.push_back(var->AsShaderResource());
+			logger::info("1.2");
 		}
 		logger::info("2");
 		for (int i = 0; i < description.nUAVs; ++i) {
